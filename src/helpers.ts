f642a4ff293d38b9eac52d3d6f0048decd7084c4
Antosik <grig_anton96@mail.ru>
2019-01-14 21:40:24 +0300

    [add] some helpers

---
+++ src/helpers.ts
@@ -0,0 +1,6 @@
+import { format as formatDate, parse as parseDate } from "date-fns";
+
+export const reformatDate = (date: string, previous: string, next: string) => formatDate(parseDate(date, previous, new Date()), next);
+export { formatDate, parseDate };
+
+export const boldIF = (string: string, condition: boolean) => condition ? `**${string}**` : string;
