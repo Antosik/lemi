540357628005920f641ed169e12df96e41041e0e
Antosik <grig_anton96@mail.ru>
2019-04-21 01:18:00 +0300

    [add] paged stage command *-*

--- src/commands/stage.ts
+++ src/commands/stage.ts
@@ -1,20 +1,21 @@
 import { format as formatDate } from "date-fns";
 import { RichEmbed } from "discord.js";
 
+import { IStageClub } from "../interfaces/IClub";
 import { ICommand } from "../interfaces/ICommand";
 import format, { consts } from "../localization";
 
+import { createPagedMessage } from "../helpers/discord";
 import { boldIF, underlineIF } from "../helpers/functions";
 
 module.exports = {
   name: "myclubstage",
   description: "Топ этапа вашего клуба.",
   aliases: ["этап", "stage", "topstage", "st"],
-  usage: "stage/этап [номер этапа (1-3)] [количество клубов]",
+  usage: "stage/этап [номер этапа (1-3)]",
 
   async execute(ctx, message, args) {
     const stage_index: number = Number(args[0]) || undefined;
-    const count: number = Number(args[1]) || 10;
 
     const [live_season, homeclub] = await Promise.all([ctx.clubs.getLiveSeason(), ctx.clubs.getHomeClub()]);
     if (!stage_index && live_season.isEnded()) {
@@ -40,27 +41,50 @@ module.exports = {
     const end_date = formatDate(stage.end_date, "dd.MM.yyyy");
     const now = formatDate(new Date(), "HH:mm:ss dd.MM.yyyy");
 
-    const result = new RichEmbed()
+    const template = new RichEmbed()
       .setColor("#0099ff")
       .setURL(`https://clubs.ru.leagueoflegends.com/rating?ssid=${live_season.id}&stid=${stage.id}`)
       .setTitle(`Рейтинг клубов`)
       .setDescription(`Сезон "${live_season.title}". Этап ${stage.number} (${start_date} - ${end_date})`)
       .setFooter(now);
 
-    stage_clubs
-      .filter((stage_club) => stage_club.rank <= count)
-      .forEach((stage_club) => {
-        const title = underlineIF(boldIF(`${stage_club.rank}. ${stage_club.club.lol_name}`, stage_club.rank <= 3), homeclub.id === stage_club.club.id);
-        const description = `${stage_club.points}pt - ${format("player", stage_club.club.members_count)}`;
-        result.addField(title, description);
-      });
+    const count = 10;
+    const pages_count = Math.ceil(stage_clubs.length / count);
+    let stage_clubs_slice = stage_clubs.slice(0, count);
+    let result = formatStage(template, stage_clubs_slice);
 
-    if (homeclub_stage.rank > count) {
-      const title = underlineIF(boldIF(`${homeclub_stage.rank}. ${homeclub_stage.club.lol_name}`, homeclub_stage.rank <= 3), homeclub.id === homeclub_stage.club.id);
-      const description = `${homeclub_stage.points}pt - ${format("player", homeclub_stage.club.members_count)}`;
-      result.addField(title, description);
+    const stage_clubs_message_r = await message.channel.send(result);
+    const stage_clubs_message = Array.isArray(stage_clubs_message_r) ? stage_clubs_message_r[0] : stage_clubs_message_r;
+
+    if (pages_count > 1) {
+      await createPagedMessage(
+        stage_clubs_message,
+        async (page, reaction) => {
+          const index_start = (page - 1) * count + 1;
+
+          stage_clubs_slice = stage_clubs.slice((page - 1) * count, page * count);
+          result = formatStage(template, stage_clubs_slice, index_start);
+          result.setFooter(`Страница ${page} • ${now}`);
+          await stage_clubs_message.edit(result);
+        },
+        {
+          filter: (r, user) => user.id === message.author.id,
+          pages_count
+        }
+      );
     }
 
-    await message.channel.send(result);
+    function formatStage(embed: RichEmbed, clubs: IStageClub[], index_start = 1): RichEmbed {
+      const res = new RichEmbed(embed);
+      res.fields = [];
+
+      clubs.forEach((club, i) => {
+        const title = underlineIF(boldIF(`${club.rank}. ${club.club.lol_name}`, club.rank <= 3), homeclub.id === club.club.id);
+        const description = `${club.points}pt - ${format("player", club.club.members_count)}`;
+        res.addField(title, description);
+      });
+
+      return res;
+    }
   }
 } as ICommand;
