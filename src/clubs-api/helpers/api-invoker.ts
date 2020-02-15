328913332af973bcb718eb27b43aa686ed2a0ef1
Antosik <grig_anton96@mail.ru>
2020-02-15 16:19:31 +0300

    misc(clubs-api): optional chaining in api-invoker

--- src/clubs-api/helpers/api-invoker.ts
+++ src/clubs-api/helpers/api-invoker.ts
@@ -21,10 +21,10 @@ export class ClubsAPIInvoker {
       .then(({ data: result }) => result)
       .catch((e) => {
         // console.log(e);
-        if (e.response && e.response.data && e.response.data.detail && e.response.data.detail === "club not selected") {
+        if (e.response?.data?.detail === "club not selected") {
           throw new Error(consts.clubNotSelected);
         }
-        if (e.response && e.response.status === 401) {
+        if (e.response?.status === 401 || e.response?.status === 403) {
           throw new Error(consts.authError);
         }
         throw new Error(consts.requestError);
